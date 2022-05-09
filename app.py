bd67c174ff3c40587d95eb173785752100d442af
pielc <pierre.le.chanu.pro@gmail.com>
2022-05-09 20:06:12 +0200

    add /usecase/API

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from crypt import methods
+from flask import Flask, request, abort
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "Hello Verteego!"
+
+@app.route("/usecase", methods=['POST'])
+def post_content():
+  if not request.json or 'usecase' not in request.json:
+    abort(400)
+  return {'usecase': request.json['usecase']}
 
 if __name__ == "__main__":
   app.run()
